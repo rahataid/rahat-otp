aee3eaf0de8f25efc6d56f6a91c048543d9f4624
anupama-rumsan <anupama@rumsan.net>
2024-05-16 12:19:46 +0545

    prabhu sms fixes

--- src/plugins/sms/prabhu.js
+++ src/plugins/sms/prabhu.js
@@ -4,17 +4,29 @@ const querystring = require('querystring');
 
 const url = config.get('services.prabhu.url');
 const token = config.get('services.prabhu.token');
+const msg = config.get('msg')
 
-module.exports = async (phone, message) => {
+module.exports = async (phone, message,otp) => {
   if (!phone) throw new Error('No receipent was specified');
   if (!message) throw new Error('No Message was specified');
+
+  if(phone.toString().slice(0,4) === '+977') phone = phone.toString().slice(4).trim();
   // return axios.post(`${url}?token=${token}`, [
   //   {
   //     Message: message,
   //     MobileNumber: phone
   //   }
   // ]);
-
-  const params = querystring.stringify({ to: phone, content: message, token });
+  const otpmsg = createMessage(otp)
+  const params = querystring.stringify({ to: phone, content: otpmsg, token });
   return axios.get(`${url}?${params}`);
 };
+
+const createMessage = async(otp) =>{
+  if(!msg) return false;
+
+  let message = msg;
+  if (msg.includes('${otp}')) message = message.replace('${otp}', otp);
+  return message;
+
+}
