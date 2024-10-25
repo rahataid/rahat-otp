09339be3d73498fa27e7f201621be75c8f3eb9d1
myanzik <myanz.470shrestha@gmail.com>
2024-10-25 10:02:14 +0700

    textsms plugins added

---
+++ src/plugins/sms/textsms.js
@@ -0,0 +1,20 @@
+const config = require('config');
+const axios = require('axios');
+
+const apiKey = config.get('services.textsms.apiKey');
+const partnerId = config.get('services.textsms.partnerId');
+const url = config.get('services.textsms.url');
+
+module.exports = async (phone, message, otp) => {
+  if (!phone) throw new Error('No receipent was specified');
+  if (!message) throw new Error('No Message was specified');
+
+  const otpmsg = `Here is your OTP to complete your transaction: ${otp}`;
+  return axios.post(url, {
+    apiKey,
+    partnerId,
+    message: otpmsg,
+    shortcode: 'TextSms',
+    mobile: phone
+  });
+};
